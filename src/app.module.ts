7c5411b56078aa35876ac2374e9e3268dfbf2c54
camaral2 <cristian_amaral@hotmail.com>
2023-04-15 11:37:00 -0300

    Debug variavel of env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -24,4 +24,18 @@ import 'dotenv/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  onModuleInit() {
+    const PORT = process.env.PORT || 4000;
+    const PORT_MCRO = process.env.PORT_MCRO || 4010;
+    const HOST_MCRO = process.env.HOST_MCRO || 'localhost';
+
+    console.log('PORT:', PORT);
+    console.log('PORT_MCRO:', PORT_MCRO);
+    console.log('HOST_MCRO:', HOST_MCRO);
+
+    Logger.log('PORT:', PORT);
+    Logger.log('PORT_MCRO:', PORT_MCRO);
+    Logger.log('HOST_MCRO:', HOST_MCRO);
+  }
+}
